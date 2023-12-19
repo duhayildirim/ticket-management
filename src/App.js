2ed062948e382301e435dd9dc247d8c41d8ebdcb
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-12-19 19:21:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom/cjs/react-router-dom';
+import Apply from './components/Apply';
+import ApplyCheck from './components/ApplyCheck';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Management from './components/Management';
+import Navbar from './components/Navbar';
+import Page404 from './components/Page404';
+import ApplyDetail from './components/ApplyDetail';
+import ApplySuccess from './components/ApplySuccess';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+          <Switch>
+            <Route path="/basvuru-olustur" component={Apply} />
+            <Route path="/404" component={Page404} />
+            <Route path="/basvuru-listesi" component={Management} />
+            <Route path="/basvuru-sorgula" component={ApplyCheck} />
+            <Route path="/admin" component={Login} />
+            <Route path="/basvuru-detay" component={ApplyDetail} />
+            <Route path="/basvuru-basarili" component={ApplySuccess} />
+          </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+
+
+
