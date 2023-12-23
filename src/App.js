01539d65e45972c9d147cbd100b6b50f2c295c41
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-12-23 21:16:48 +0300

    create userContext

--- src/App.js
+++ src/App.js
@@ -9,26 +9,29 @@ import Page404 from './components/Page404';
 import ApplyDetail from './components/ApplyDetail';
 import ApplySuccess from './components/ApplySuccess';
 import { ApplicationProvider } from './context/ApplicationContext';
+import { UserProvider } from './context/UserContext';
 
 function App() {
     return (
         <>
-            <ApplicationProvider>
-                <Router>
-                    <Navbar />
-                    <Switch>
-                        <Redirect exact from="/" to="/basvuru-olustur" />
-                        <Route exact path="/basvuru-olustur" component={Apply} />
-                        <Route path="/basvuru-listesi" component={Management} />
-                        <Route path="/basvuru-sorgula" component={ApplyCheck} />
-                        <Route path="/admin" component={Login} />
-                        <Route path="/basvuru-detay/:code" component={ApplyDetail} />
-                        <Route path="/basvuru-basarili" component={ApplySuccess} />
-                        <Route path="*" component={Page404} />
-                    </Switch>
-                    <Footer />
-                </Router>
-            </ApplicationProvider>
+            <UserProvider>
+                <ApplicationProvider>
+                    <Router>
+                        <Navbar />
+                        <Switch>
+                            <Redirect exact from="/" to="/basvuru-olustur" />
+                            <Route exact path="/basvuru-olustur" component={Apply} />
+                            <Route path="/basvuru-listesi" component={Management} />
+                            <Route path="/basvuru-sorgula" component={ApplyCheck} />
+                            <Route path="/admin" component={Login} />
+                            <Route path="/basvuru-detay/:code" component={ApplyDetail} />
+                            <Route path="/basvuru-basarili" component={ApplySuccess} />
+                            <Route path="*" component={Page404} />
+                        </Switch>
+                        <Footer />
+                    </Router>
+                </ApplicationProvider>
+            </UserProvider>
         </>
     );
 }
---
+++ src/context/UserContext.js
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect } from 'react';
+
+const UserContext = createContext();
+
+const localUsername = localStorage.getItem('username') || 'kodluyoruz'; 
+const localPassword = localStorage.getItem('password') || 'bootcamp109';
+const localIsActive = localStorage.getItem('isActive') || false;
+
+export const UserProvider = ({ children }) => {
+    const [ username, setUsername ] = useState(localUsername);
+    const [ password, setPassword ] = useState(localPassword);
+    const [ isActive, setIsActive ] = useState(localIsActive);
+
+    useEffect(() => {
+        if (!localStorage.getItem('username')) {
+            localStorage.setItem('username', username);
+        }
+        if (!localStorage.getItem('password')) {
+            localStorage.setItem('password', password);
+        }
+        if (!localStorage.getItem('isActive')) {
+            localStorage.setItem('isActive', isActive.toString());
+        }
+    }, [username, password, isActive]);
+
+    const values = {
+        username,
+        password,
+        isActive,
+        setUsername,
+        setPassword,
+        setIsActive,
+    };
+
+    return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
+};
